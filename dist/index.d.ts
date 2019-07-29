63134788de3bc44c7dd3c9577849e6c069c5ecdb
realfresh <cpatarun@gmail.com>
2019-07-29 23:30:11 +0800

    remove dist from github

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { EzAuth } from "./core";
-import { EzAuthMongoDBAdapter } from "./databases/mongo";
-export { EzAuthMongoDBAdapter };
-export default EzAuth;
--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const core_1 = require("./core");
-const mongo_1 = require("./databases/mongo");
-exports.EzAuthMongoDBAdapter = mongo_1.EzAuthMongoDBAdapter;
-exports.default = core_1.EzAuth;
-//# sourceMappingURL=index.js.map
